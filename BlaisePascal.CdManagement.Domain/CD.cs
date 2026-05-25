72e85d9fa7fa89b4c93f1b8d68d1f277968cbe28
Alberto Mortani <alberto.mortani.stud@ispascalcomandini.it>
2026-05-25 22:15:23 +0200

    Added CD

--- BlaisePascal.CdManagement.Domain/CD.cs
+++ BlaisePascal.CdManagement.Domain/CD.cs
@@ -1,12 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BlaisePascal.CdManagement.Domain
 {
-    internal class CD
+    public class CD
     {
+        // Properties
+        public List<Song> Songs { get; private set; }
+        private string Author { get; set; }
+        private string Title { get; set; }
+
+        // Constructor
+        public CD(string author, string title, List<Song> songs) 
+        {
+            Author = author;
+            Title = title;
+            Songs = songs;
+        }
+
+        // Getter
+        public string GetTitle() => Title;
+        public string GetAuthor() => Author;
+
+        // Setter
+        public void SetAuthor(string author) { Author = author; }
+        public void SetTitle(string title) { Title = title; }
+
+
+        // Methods
+        public override string ToString()
+        {
+            string msg = "";
+            foreach(Song s in Songs)
+            {
+                msg += s.GetTitle;
+                msg += "\n";
+            }
+
+            return msg;
+        }
+
+        public int GetDuration()
+        {
+            int totDuration = 0;
+            foreach(Song s in Songs)
+            {
+                totDuration += s.GetDuration();
+            }
+            return totDuration;
+        }
     }
 }
